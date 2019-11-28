a84b961ef5c70c17020e006156573fdf57926407
Sam Stoelinga <sammiestoel@gmail.com>
2019-11-28 14:32:01 -0800

    Added assertion to poller_test

--- pkg/poller/poller_test.go
+++ pkg/poller/poller_test.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"path/filepath"
+	"reflect"
+	"runtime"
 	"testing"
 )
 
@@ -13,9 +16,19 @@ func check(e error) {
 	}
 }
 
+// equals fails the test if exp is not equal to act.
+func equals(tb testing.TB, exp, act interface{}) {
+	if !reflect.DeepEqual(exp, act) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
+		tb.FailNow()
+	}
+}
+
 func TestParseNetworkMetrics(t *testing.T) {
 	json, err := ioutil.ReadFile("../../test/stats-summary-1.json")
 	check(err)
 	s := ParseNetworkMetrics(json)
+	equals(t, s[0].Name, "network-metering-agent-bljqp")
 	fmt.Println(s)
 }
