d337f49bf099eeff84e57767d1a82f7be1336159
Sam Stoelinga <sammiestoel@gmail.com>
2019-12-05 13:35:17 -0800

    Add function to calculate throughput

--- pkg/poller/poller.go
+++ pkg/poller/poller.go
@@ -5,13 +5,15 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 type PodNetworkStats struct {
-	Name      string `json:"name"`
-	Namespace string `json:"namespace"`
-	RxBytes   int64  `json:"rxBytes"`
-	TxBytes   int64  `json:"txBytes"`
+	Name      string    `json:"name"`
+	Namespace string    `json:"namespace"`
+	RxBytes   int64     `json:"rxBytes"`
+	TxBytes   int64     `json:"txBytes"`
+	Time      time.Time `json:"time"`
 }
 
 func GetMetrics(url string) []byte {
@@ -33,14 +35,25 @@ func ParseNetworkMetrics(json []byte) []PodNetworkStats {
 	networkMetrics := make([]PodNetworkStats, podCount)
 	i := 0
 	pods.ForEach(func(key, value gjson.Result) bool {
+		networkTime, _ := time.Parse(time.RFC3339, value.Get("network.time").String())
 		s := PodNetworkStats{
 			Name:      value.Get("podRef.name").String(),
 			Namespace: value.Get("podRef.namespace").String(),
 			RxBytes:   value.Get("network.rxBytes").Int(),
-			TxBytes:   value.Get("network.txBytes").Int()}
+			TxBytes:   value.Get("network.txBytes").Int(),
+			Time:      networkTime}
 		networkMetrics[i] = s
 		i++
 		return true // keep iterating
 	})
 	return networkMetrics
+}
+
+// NetworkThoughput calculates the ingress and egress thoughput using 2 datapoints using the formula
+// (latest - old) / (delta in time between latest and old)
+func NetworkThroughput(old, latest PodNetworkStats) (rx, tx float64) {
+	timeDelta := (latest.Time.Sub(old.Time).Seconds())
+	rx = float64(latest.RxBytes-old.RxBytes) / timeDelta
+	tx = float64(latest.TxBytes-old.TxBytes) / timeDelta
+	return rx, tx
 }
