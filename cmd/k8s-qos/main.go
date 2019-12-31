8ad0efaaaf20d06988a0a532d802e8ed08dda323
Sam Stoelinga <sammiestoel@gmail.com>
2019-12-31 00:13:09 -0800

    updated executable to pull and parse pods

--- cmd/k8s-qos/main.go
+++ cmd/k8s-qos/main.go
@@ -8,16 +8,18 @@ import (
 )
 
 func main() {
-	interval := flag.Int("interval", 10, "Interval to pull metrics from kubelet")
-	url := "http://localhost:10255/stats/summary"
-	kubeletUrl := flag.String("url", url, "URL to kubelet stats/summary endpoint")
+	interval := flag.Int("interval", 10, "Interval to pull data from kubelet")
+	url := "http://localhost:10255"
+	kubeletUrl := flag.String("url", url, "URL to kubelet api endpoint")
 	flag.Parse()
 	c := time.Tick(time.Second * time.Duration(*interval))
 	for _ = range c {
-		fmt.Println("Getting metrics from URL:", *kubeletUrl)
-		json := poller.GetURL(*kubeletUrl)
+		json := poller.GetURL(*kubeletUrl + "/stats/summary")
 		metrics := poller.ParseNetworkMetrics(json)
 		fmt.Println(metrics)
+		json = poller.GetURL(*kubeletUrl + "/pods")
+		pods := poller.ParsePods(json)
+		fmt.Println(pods)
 	}
 	fmt.Println("Exitting")
 }
